a44e1c2ff9f8f0922ccb3a054f289ed0f5d7a6e8
unknown <hamzanazizi72@gmail.com>
2020-01-31 00:55:03 -0800

    firs commit

---
+++ azizi/application/views/home/dashboard.php
@@ -0,0 +1,9 @@
+<H4><marquee >BIO DATA</marquee></H4>
+<div class="text-center">
+  <img src="<?= base_url()?>templates/dist/img/azizi.jpg" style="width:250px" class="rounded-circle" alt="...">
+<h3 class ="text-center">Nama : Hamzan Azizi</h3>
+<p class = "text-center">NIM : TI14170014</p>
+<p class = "text-center">alamat : Gbk. Rejeng Desa Mertak Tombok</p>
+<p class = "text-center">STMIK LOMBOK</p>
+<p class = "text-center">Prodi : Teknik Informatika</p>
+</div>
